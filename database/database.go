dc9372fea4d344d038a9278cb983b5265f36b379
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-12-22 17:38:03 -0200

    using config parameters to connect to database

--- database/database.go
+++ database/database.go
@@ -3,16 +3,27 @@ package database
 import (
     "fmt"
     "github.com/jinzhu/gorm"
+    "github.com/fiscaluno/fiscaluno-api/config"
     _ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 var db *gorm.DB
 
 func GetInstance() (*gorm.DB) {
-    db, err := gorm.Open("mysql", "user:password@/database_name?charset=utf8&parseTime=True&loc=Local")
-    
-    if err != nil {
-        fmt.Print("error at database connection")
+    if db == nil { 
+        var err interface {}
+        
+        user := config.GetConfig("db_user")
+        password := config.GetConfig("db_password")
+        database := config.GetConfig("db_name")
+
+        connect := fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local", user, password, database)
+
+        db, err = gorm.Open("mysql", connect)
+        
+        if err != nil {
+            fmt.Print("error at database connection")
+        }
     }
 
     return db
