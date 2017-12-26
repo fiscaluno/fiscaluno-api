0d27f653b339c7ee8690143884a998bc601aebd5
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-12-26 10:27:54 -0200

    renamed some files

--- routes/students.go
+++ routes/students.go
@@ -2,7 +2,7 @@ package routes
 
 func init() {  
 
-    users_routes := app.Party("/users", userMiddleware)
+    users_routes := app.Party("/students", userMiddleware)
 
     {
         users_routes.Get("/id", teste)
