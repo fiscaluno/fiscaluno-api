c17836ffb9b96a2a7869272be89d4d653a27b310
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-12-26 15:53:21 -0200

    saving error and trace logs at log file

--- fiscalog/fiscalog.go
+++ fiscalog/fiscalog.go
@@ -1,32 +1,47 @@
 package fiscalog
 
 import (
-    "os"
-    "log"
-    "io/ioutil"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 var (
-    Trace   *log.Logger
-    Info    *log.Logger
-    Warning *log.Logger
-    Error   *log.Logger
+	Trace   *log.Logger
+	Info    *log.Logger
+	Warning *log.Logger
+	Error   *log.Logger
 )
 
 func init() {
-    Trace = log.New(ioutil.Discard,
-        "TRACE: ",
-        log.Ldate|log.Ltime|log.Lshortfile)
+	currPath, err := os.Getwd()
 
-    Info = log.New(os.Stdout,
-        "INFO: ",
-        log.Ldate|log.Ltime|log.Lshortfile)
+	if err != nil {
+		log.Fatalln("Failed to get current dir: ", err)
+	}
 
-    Warning = log.New(os.Stdout,
-        "WARNING: ",
-        log.Ldate|log.Ltime|log.Lshortfile)
+	logFilePath := currPath + "/fiscalog/fiscaluno.log"
+	logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 
-    Error = log.New(os.Stderr,
-        "ERROR: ",
-        log.Ldate|log.Ltime|log.Lshortfile)
+	if err != nil {
+		log.Fatalln("Failed to open log file: ", err)
+	}
+
+	Trace = log.New(io.MultiWriter(logFile, ioutil.Discard),
+		"TRACE: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Info = log.New(os.Stdout,
+		"INFO: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Warning = log.New(os.Stdout,
+		"WARNING: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Error = log.New(io.MultiWriter(logFile, os.Stderr),
+		"ERROR: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
 }
