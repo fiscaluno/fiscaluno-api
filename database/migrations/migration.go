bfaa8cef92f18329d17f459deb0a9bf2b1b16226
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-12-26 11:51:58 -0200

    some migration outputs and check if table exists

--- database/migrations/migration.go
+++ database/migrations/migration.go
@@ -1,13 +1,34 @@
 package migrations
 
 import (
+    "fmt"
+    "regexp"
+    "reflect"
     "github.com/fiscaluno/fiscaluno-api/database"
 )
 
 // Database Instance
 var db = database.GetInstance()
 
+func init() {
+    fmt.Println("[AutoMigration: Migrating database... Wait a moment please]")
+}
+
+func Migrate() {
+    fmt.Println("[AutoMigration: Database migrated]")
+}
+
 // Create new Schema to database
 func Create(Schema interface {}) {
-    db.AutoMigrate(Schema)
+    pattern := regexp.MustCompile(`\.(\w+)$`)
+    schemaString := reflect.TypeOf(Schema).String()
+    tableName := pattern.FindStringSubmatch(schemaString)[1]
+
+    if !db.HasTable(Schema) {
+        fmt.Println(fmt.Sprintf("[AutoMigration: Creating %s table...]", tableName))
+        defer fmt.Println("[AutoMigration: Students table created]")
+        db.AutoMigrate(Schema)
+    } else {
+        fmt.Println(fmt.Sprintf("[AutoMigration: %s table exists...]", tableName))
+    }
 }
