0811c8b1adbd425d4ccc8757eb6edb031a9e17b9
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-12-27 11:11:48 -0200

    go fmt

--- models/model.go
+++ models/model.go
@@ -1,16 +1,16 @@
 package models
 
 import (
-    "github.com/fiscaluno/fiscaluno-api/database"
+	"github.com/fiscaluno/fiscaluno-api/database"
 )
 
 var db = database.GetInstance()
 
 // Model contract
 type Model interface {
-    All()
-    Find()
-    Save()
-    Delete()
-    Update()
+	All()
+	Find()
+	Save()
+	Delete()
+	Update()
 }
