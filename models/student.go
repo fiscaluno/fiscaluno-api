a0a88dd307bdb7bc3524c407b101849b8cdb7f60
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-12-27 10:56:20 -0200

    go fmt

--- models/student.go
+++ models/student.go
@@ -1,26 +1,26 @@
 package models
 
 import (
+	"github.com/fiscaluno/fiscaluno-api/database"
 	"github.com/jinzhu/gorm"
 	"github.com/mitchellh/mapstructure"
-	"github.com/fiscaluno/fiscaluno-api/database"
 )
 
 var db = database.GetInstance()
 
 type Student struct {
 	gorm.Model
-	Name        string `gorm:"size:50"` 
-	Phone       string    `gorm:"size:10"`
-	Institution string    `gorm:"size:50"`
-	Course      string    `gorm:"size:50"`
-	Email       string    `gorm:"size:50"`
+	Name        string `gorm:"size:50"`
+	Phone       string `gorm:"size:10"`
+	Institution string `gorm:"size:50"`
+	Course      string `gorm:"size:50"`
+	Email       string `gorm:"size:50"`
 }
 
-func CreateStudent(studentAttributes map[string] interface {}) (*Student) {
+func CreateStudent(studentAttributes map[string]interface{}) *Student {
 	var student Student
 	mapstructure.Decode(studentAttributes, &student)
-	
+
 	return &student
 }
 
