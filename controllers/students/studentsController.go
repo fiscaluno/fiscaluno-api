f49d313e547485f08a11e9722e9d6ea4c4b97fb0
Vinicius Luiz <viniciuslcp97@gmail.com>
2018-01-02 09:38:53 -0200

    go fmt

--- controllers/students/studentsController.go
+++ controllers/students/studentsController.go
@@ -1,10 +1,10 @@
 package students
 
 import (
-    "strconv"
-    "reflect"
 	student "github.com/fiscaluno/fiscaluno-api/models/student"
 	"github.com/kataras/iris"
+	"reflect"
+	"strconv"
 )
 
 // Creates new student - POST Method
@@ -13,12 +13,12 @@ func InsertStudent(ctx iris.Context) {
 	values := ctx.FormValues()
 
 	for key, value := range values {
-        intVal, err := strconv.Atoi(value[0])
-        if err != nil {
-          attributes[key] = value[0]
-        }else {
-		  attributes[key] = intVal
-        }
+		intVal, err := strconv.Atoi(value[0])
+		if err != nil {
+			attributes[key] = value[0]
+		} else {
+			attributes[key] = intVal
+		}
 	}
 
 	student.Create(attributes).Save()
@@ -26,16 +26,16 @@ func InsertStudent(ctx iris.Context) {
 
 // Find student at database - GET Method
 func FindById(ctx iris.Context) {
-    id, err := ctx.Params().GetInt("id")
-    
-    if err != nil {
-        ctx.JSON(err)
-    }
-    
-    // Checks if student exists at database
-    if reflect.DeepEqual(student.Student{}, *student.Find(id)) {
-        ctx.StatusCode(404)
-    } else {
-        ctx.JSON(student.Find(id))    
-    }
+	id, err := ctx.Params().GetInt("id")
+
+	if err != nil {
+		ctx.JSON(err)
+	}
+
+	// Checks if student exists at database
+	if reflect.DeepEqual(student.Student{}, *student.Find(id)) {
+		ctx.StatusCode(404)
+	} else {
+		ctx.JSON(student.Find(id))
+	}
 }
