a8f3402b951fcd90a1c7d1baf6499f7942941b3b
Vinicius Luiz <viniciuslcp97@gmail.com>
2018-01-02 10:06:21 -0200

    get institution by id route

--- controllers/institutions/institutionsController.go
+++ controllers/institutions/institutionsController.go
@@ -2,9 +2,27 @@ package institutions
 
 import (
 	institution "github.com/fiscaluno/fiscaluno-api/models/institution"
-	"github.com/kataras/iris"
+    "github.com/kataras/iris"
+	"reflect"
 )
 
+// List of all institutions - GET Method
 func AllInstitutions(ctx iris.Context) {
 	ctx.JSON(institution.All())
+}
+
+// Institution informations - GET Method
+func InstitutionById(ctx iris.Context) {
+    id, err := ctx.Params().GetInt("id")
+
+    if err != nil {
+        ctx.JSON(err)
+    }
+
+    // Checks if student exists at database
+    if reflect.DeepEqual(institution.Institution{}, *institution.Find(id)) {
+        ctx.StatusCode(404)
+    } else {
+        ctx.JSON(institution.Find(id))
+    }
 }
