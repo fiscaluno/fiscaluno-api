95be9fb564f8110f45570c026d092f238a0002ff
Vinicius Luiz <viniciuslcp97@gmail.com>
2018-01-02 15:03:20 -0200

    detailed review migration

---
+++ database/migrations/detailed_review_migration.go
@@ -0,0 +1,11 @@
+package migrations
+
+import (
+    model "github.com/fiscaluno/fiscaluno-api/models"
+)
+
+var DetailedReview = model.DetailedReview{}
+
+func init() {
+    Create(&DetailedReview)
+}
