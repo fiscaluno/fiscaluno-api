3544f3f22bbc00727fc818d7c1991d3d9e0a79d5
Vinicius Luiz <viniciuslcp97@gmail.com>
2018-01-08 15:35:44 -0200

    handler migrating database

--- handler/handler.go
+++ handler/handler.go
@@ -1,11 +1,10 @@
 package handler
 
 import (
+    "fmt"
     "os"
     "github.com/urfave/cli"
-    "fmt"
-    _"github.com/fiscaluno/fiscaluno-api/database/migrations"
-    "go/importer"
+    "github.com/fiscaluno/fiscaluno-api/database/migrations"
 )
 
 var app *cli.App 
@@ -15,22 +14,15 @@ func Handle() {
     app.Name = "Fiscaluno"
     app.Usage = "Fiscaluno Command Handler"
     app.Action = handleCommand
-
     app.Run(os.Args)
 }
 
 func handleCommand(c *cli.Context) error {
-    if c.Args().Get(0) == "migrate" {
-        MigrateDatabase()
-        return nil
+    command := c.Args().Get(0)
+    if command == "migrate" {
+        migrations.Migrate()
+    } else if command == "teste" {
+        fmt.Println("teste")
     }
     return nil
-}
-
-func MigrateDatabase() {
-    _, err := importer.Default().Import("github.com/fiscaluno/fiscaluno-api/database/migrations")
-    if err != nil {
-        fmt.Printf("error: %s\n", err.Error())
-        return
-    }
 }
